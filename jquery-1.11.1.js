2a996896548acaa95f60844f3ef02054bcd50a21
Todd Anderson <toddwords@gmail.com>
2017-02-10 13:56:09 -0500

    yes

--- jquery-1.11.1.js
+++ jquery-1.11.1.js
@@ -1,4 +1,5 @@
 
 jQuery(document).ready(function($){
-$('h1,h2,h3,p').text("zXxxzZhelloZebulonzxeXXz"+$(this).text())
+$('h1,h2,h3,p').each(function(){
+	$(this).text("zXxxzZhelloZebulonzxeXXz"+$(this).text())
 });
